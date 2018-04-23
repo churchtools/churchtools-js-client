dc4e278776db2172183aa888d0fe02567f250566
David Schilling <dschilling@churchtools.de>
2018-04-23 11:46:00 +0200

    First library version

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'churchtools-client.js',
+        library: 'churchtoolsClient'
     },
     mode: 'production',
     module: {
