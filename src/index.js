1c198f2c3db5e5133e320e884593d37786fbc105
Jan Pawellek <jpawellek@churchtools.de>
2020-09-25 15:10:52 +0200

    Remove churchtoolsApi.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import * as churchtoolsClient from './churchtoolsClient';
-import * as churchtoolsApi from './churchtoolsApi';
 import { activateLogging } from './logging';
 import * as urlHelper from './urlHelper';
 
-export { churchtoolsClient, churchtoolsApi, activateLogging, urlHelper };
+export { churchtoolsClient, activateLogging, urlHelper };
