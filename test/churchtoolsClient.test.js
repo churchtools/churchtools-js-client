f82a8750e4f0e90b447e94208619a33bd64ed84a
Matthias Huber <hubermat@gmx.de>
2022-06-14 20:19:34 +0200

    use get with deferred

--- test/churchtoolsClient.test.js
+++ test/churchtoolsClient.test.js
@@ -4,7 +4,7 @@ test('should fail for invalid credentials', done => {
     let ctc = new ChurchToolsClient('https://review.church.tools', 'foobar', true);
     //ctc.setRateLimitInterceptor();
     //ctc.setCookieJar(axiosCookieJarSupport.default, new tough.CookieJar());
-    ctc.get('/contactlabels', {}, false, false)
+    ctc.get('/contactlabels')
         .then(result => {
             expect(true).toBe(false);
             done();
